2a64b11d93229170a52b0b483857f116b210243c
lenabojc81 <118843309+lenabojc81@users.noreply.github.com>
2024-11-13 19:59:08 +0100

    Feature/delete transaction (#15)
    * delete a transaction
    
    * edit transaction

--- backend/src/transactions.js
+++ backend/src/transactions.js
@@ -26,4 +26,36 @@ router.post('/newTransaction', async (req, res) => {
     }
 });
 
+router.delete('/deleteTransaction/:id', async (req, res) => {
+    try {
+        await Transaction.findByIdAndDelete(req.params.id);
+        console.log('transaction deleted');
+        res.status(200).send('transaction deleted');
+    } catch(err) {
+        console.error(err);
+        res.status(400).send({error: err });
+    }
+});
+
+router.put('/editTransaction/:id', async (req, res) => {
+    try {
+        const updatedTransaction = await Transaction.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        if (!updatedTransaction) {
+            return res.status(404).send({ error: 'Transaction not found' });
+        }
+
+        console.log('Transaction successfully updated');
+        res.status(200).json(updatedTransaction);
+    } catch (err) {
+        console.error(err);
+        res.status(400).send({ error: 'Failed to update transaction' });
+    }
+});
+
+
 export default router;
